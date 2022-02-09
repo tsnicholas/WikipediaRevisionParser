0eb4035d3d07c3ec4f0315aea6a88a3e16f6d84d
Pirate <Timnich2001@Gmail.com>
2022-02-08 20:18:52 -0500

    Adds a parse function into revisionParser

--- src/main/java/edu/bsu/cs222/model/revisionParser.java
+++ src/main/java/edu/bsu/cs222/model/revisionParser.java
@@ -1,5 +1,15 @@
 package edu.bsu.cs222.model;
 
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class revisionParser {
 
+    public void parse(InputStream inputStream) throws IOException {
+        JSONArray username = JsonPath.read(inputStream, "$..user");
+        JSONArray timestamp = JsonPath.read(inputStream, "$..timestamp");
+    }
 }
--- src/main/java/edu/bsu/cs222/model/wikiPage.java
+++ src/main/java/edu/bsu/cs222/model/wikiPage.java
@@ -10,23 +10,28 @@ import java.net.URLConnection;
 
 public class wikiPage {
     private final URL wikiURL;
+    private URLConnection connection;
 
     public wikiPage(String wikiName) throws MalformedURLException {
         wikiURL = new URL("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" + wikiName + "&rvprop=timestamp|user&rvlimit=4&redirects");
     }
 
-    // TODO: Should throw some kind of error when there's a network error
     public void getRevisions() {
         try {
-            URLConnection connection = wikiURL.openConnection();
+            connection = wikiURL.openConnection();
             connection.setRequestProperty("User-Agent",
                     "Revision Reporter/0.1 (http://www.cs.bsu.edu/~pvg/courses/cs222Sp22; tsnicholas@bsu.edu, acmiller@bsu.edu)");
-            InputStream inputStream = connection.getInputStream();
-            revisionParser parser = new revisionParser();
+            parseRevisions();
         }
         catch (IOException e) {
             ErrorWindow error = new ErrorWindow("An error has occurred.");
             error.displayError();
         }
+    }
+
+    private void parseRevisions() throws IOException {
+        InputStream inputStream = connection.getInputStream();
+        revisionParser parser = new revisionParser();
+        parser.parse(inputStream);
     }
 }
