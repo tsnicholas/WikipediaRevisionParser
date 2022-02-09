170e5a0d100b273fdf3c840afc4434438c1ec44d
Pirate <Timnich2001@Gmail.com>
2022-02-08 21:44:45 -0500

    Revert "Adds a new icon for the window"
    This reverts commit 0afb15eaa5e4abf73f512835a6e37d6260b04433.

--- src/main/java/edu/bsu/cs222/view/MainWindow.java
+++ src/main/java/edu/bsu/cs222/view/MainWindow.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -26,7 +25,6 @@ public class MainWindow extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setScene(new Scene(createUI()));
         primaryStage.setTitle("Wikipedia Revision Getter");
-        primaryStage.getIcons().add(new Image("Wikipedia Icon.jpg"));
         primaryStage.setWidth(500.0);
         primaryStage.show();
     }
