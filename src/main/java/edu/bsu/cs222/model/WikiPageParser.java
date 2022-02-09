c55100b301e9f31e1b246c8199856a0c644cb85d
Pirate <Timnich2001@Gmail.com>
2022-02-09 17:35:10 -0500

    Adds retrieveData()

--- src/main/java/edu/bsu/cs222/model/WikiPageParser.java
+++ src/main/java/edu/bsu/cs222/model/WikiPageParser.java
@@ -5,22 +5,14 @@ import net.minidev.json.JSONArray;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 
 public class WikiPageParser {
-    private HashMap<String, String> data;
-    private JSONArray timestamps;
-    private JSONArray users;
 
-    public HashMap<String, String> parse(InputStream testDataStream) throws IOException {
-        data = new HashMap<String, String>();
-        timestamps = (JSONArray) JsonPath.read(testDataStream, "$..timestamp");
-        users = (JSONArray) JsonPath.read(testDataStream, "$..user");
+    public JSONArray parseTimestamps(InputStream testDataStream) throws IOException {
+        return JsonPath.read(testDataStream, "$..timestamp");
+    }
 
-        for(int i = 0; i < timestamps.toArray().length - 1; i++) {
-            data.put(timestamps.get(i).toString(), users.get(i).toString());
-        }
-
-        return data;
+    public JSONArray parseUsers(InputStream wikiRevisionData) throws IOException {
+        return JsonPath.read(wikiRevisionData, "$..users");
     }
 }
